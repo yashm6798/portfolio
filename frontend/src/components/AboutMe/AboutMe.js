e87723fc9bf16205017bf7a64d4890366886b8d9
Yash Mehta <yashmehta@yashs-mbp-2.myfiosgateway.com>
2021-03-04 18:58:22 -0500

    Removed single page layout

--- frontend/src/components/AboutMe/AboutMe.js
+++ frontend/src/components/AboutMe/AboutMe.js
@@ -2,54 +2,38 @@ import React, { Component } from 'react';
 import Educations from '../Educations/Educations'
 import WorkExperiences from '../WorkExperiences/WorkExperiences'
 import charlie from '../../profile_image/charlie.jpg'
-import { SectionsContainer, Section } from 'react-fullpage';
 
 import './AboutMe.css'
 
 class aboutMe extends Component {
 
     render() {
-        let options = {
-            sectionClassName: 'section',
-            anchors: ['About-Me', 'Education', 'Work-Experience'],
-            scrollBar: false,
-            navigation: true,
-            verticalAlign: false
-        }
         const x = window.matchMedia("(min-width: 768px)")
         return (
             <div className="aboutMeContainer">
                 { x.matches ? (
                     <div className="laptop">
-                        <SectionsContainer className="container" {...options}>
-                            <Section scrollBar= "false" color="#062f4f">
-                                <div className="aboutMe">
-                                    <div className="photo">
-                                        <img src={charlie} alt="Charlie and Me"></img>
-                                    </div>
-                                    <div className="text">
-                                        <div className="about_me_header">
-                                            <h2>About Me</h2>
-                                            <div dangerouslySetInnerHTML={{ __html: this.props.profile.detailed_bio }}></div>
-                                        </div>
-                                    </div>
+                        <div className="aboutMe">
+                            <div className="photo">
+                                <img src={charlie} alt="Charlie and Me"></img>
+                            </div>
+                            <div className="text">
+                                <div className="about_me_header">
+                                    <h2>About Me</h2>
+                                    <div dangerouslySetInnerHTML={{ __html: this.props.profile.detailed_bio }}></div>
                                 </div>
-                            </Section>
+                            </div>
+                        </div>
 
-                            <Section scrollBar= "false" color="#813772">
-                                <div className="educations">
-                                    <h2>Education</h2>
-                                    <Educations education={this.props.education} />
-                                </div>
-                            </Section>
+                        <div className="educations">
+                            <h2>Education</h2>
+                            <Educations education={this.props.education} />
+                        </div>
 
-                            <Section scrollBar= "false" color="#8b1c00">
-                                <div className="workExperiences">
-                                    <h2>Professional Experience</h2>
-                                    <WorkExperiences workExperiences={this.props.workExperience} />
-                                </div>
-                            </Section>
-                        </SectionsContainer>
+                        <div className="workExperiences">
+                            <h2>Professional Experience</h2>
+                            <WorkExperiences workExperiences={this.props.workExperience} />
+                        </div>
                     </div>
                 ) : (
                         <div className="mobile">
